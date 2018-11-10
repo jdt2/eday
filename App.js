373751e07e89b857b25ee1c37bfa19c0a9991ee7
Jesse Du <jdt2@k12albemarle.org>
2018-11-10 11:20:11 -0500

    First working verison

--- App.js
+++ App.js
@@ -5,9 +5,11 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Image source={require('./assets/begin_quote.png')} style={{width: 80, height: 50}}/>
-        <Text style={styles.quote}>Innovation distinguishes between a leader and a follower</Text>
-        <Image source={require('./assets/end_quote.png')} style={{width: 80, height: 50}}/>
+        <View style={styles.whole}>
+          <Image source={require('./assets/begin_quote.png')} style={{width: 30, height: 20}}/>
+          <Text style={styles.quote}>Innovation distinguishes between a leader and a follower</Text>
+          {/*<Image source={require('./assets/end_quote.png')} style={{width: 30, height: 20}}/>*/}
+        </View>
         <Text style={styles.from}>Steve Jobs</Text>
       </View>
     );
@@ -20,6 +22,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  whole: {
+    flexDirection: 'row',
+    flexWrap: 'wrap'
   },
   quote: {
     width: 300,
